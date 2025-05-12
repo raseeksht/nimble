fb2031c0a8641d0e3c3b7a46bb5d2296e809d4e1
raseeksht <raseeksht1@gmail.com>
2025-05-12 23:07:51 +0545

    cron with discord webhook

--- cron.js
+++ cron.js
@@ -1,20 +1,30 @@
 
 import cron from 'node-cron';
 import { SubmitAttendance } from './a.js';
+import axios from 'axios';
 
 console.log("cron job scheduled");
 
 cron.schedule('0 23 * * *', async () => {
-    SubmitAttendance(process.env.CLOUDUSERNAME,process.env.PASSWORD,'Out')
-    
+    const resp = await SubmitAttendance(process.env.CLOUDUSERNAME,process.env.PASSWORD,'Out')
+    discord(resp)
     console.log('Out Attendance Submitted..');
 });
 
 cron.schedule('0 14 * * *', async () => {
-    SubmitAttendance(process.env.CLOUDUSERNAME,process.env.PASSWORD,'In')
-    
+    const resp = await SubmitAttendance(process.env.CLOUDUSERNAME,process.env.PASSWORD,'In')
+    discord(resp)
     console.log('In Attendance Submitted..');
 });
 
 
-  
+const discord = async (msg) => {
+    const res = await axios.post(process.env.WEBHOOK,{content:JSON.stringify(msg)});
+    console.log(res.data)
+}
+
+// const main = async () => {
+//     discord(await SubmitAttendance('a','b','c'));
+
+// }
+// main();
