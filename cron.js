4e9f629bf025cd9431c43b31fa56f6c1dcde15dd
raseeksht <raseeksht1@gmail.com>
2025-05-23 14:15:45 +0545

    cron exp:

--- cron.js
+++ cron.js
@@ -2,16 +2,22 @@
 import cron from 'node-cron';
 import { SubmitAttendance } from './a.js';
 import axios from 'axios';
+import cronstrue from 'cronstrue';
+
+const inCron = '0 14 * * 1,2';
+const outCron = '4 23 * * 1,2';
 
 console.log("cron job scheduled");
+console.log(cronstrue.toString(inCron),'In time')
+console.log(cronstrue.toString(outCron),"Out Time")
 
-cron.schedule('0 23 * * *', async () => {
+cron.schedule(outCron, async () => {
     const resp = await SubmitAttendance(process.env.CLOUDUSERNAME,process.env.PASSWORD,'Out')
     discord(resp)
     console.log('Out Attendance Submitted..');
 });
 
-cron.schedule('0 14 * * *', async () => {
+cron.schedule(inCron, async () => {
     const resp = await SubmitAttendance(process.env.CLOUDUSERNAME,process.env.PASSWORD,'In')
     discord(resp)
     console.log('In Attendance Submitted..');
@@ -23,8 +29,9 @@ const discord = async (msg) => {
     console.log(res.data)
 }
 
-// const main = async () => {
-//     discord(await SubmitAttendance('a','b','c'));
+const main = async () => {
+    // discord(await SubmitAttendance('a','b','c'));
+    // const resp = await SubmitAttendance(process.env.CLOUDUSERNAME,process.env.PASSWORD,'In')
 
-// }
-// main();
+
+}
